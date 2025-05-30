305859f020ac8ab25bc124d4c956b4af04909cb2
kushxu23 <kushxu@gmail.com>
2025-05-30 18:31:57 +0530

    opertor in java

--- javabasic.java
+++ javabasic.java
@@ -1,5 +1,6 @@
  import java.util.*;
   public class javabasic {
+    // opertor in java 
     public static void main(String args []   ) {
        int A = 10 ;
        int B  = 5 ;
@@ -8,6 +9,7 @@
          System.out.println(A *B );
           System.out.println( A/B);
            System.out.println( A%B);
+            System.out.println(A*A);
 
     }
 
